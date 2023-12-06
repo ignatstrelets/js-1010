df0e9a1ceef94d0356affc9400c85b9092ecebef
ignatstrelets <120760559+ignatstrelets@users.noreply.github.com>
2023-12-07 01:39:03 +0300

    edit js

--- js/1010_abx.js
+++ js/1010_abx.js
@@ -11,10 +11,23 @@ function verify_abx() {
   }
 }
 
-function send() {
+function send_query() {
   var a = parseInt(document.getElementById("a").value);
   var b = parseInt(document.getElementById("b").value);
   var x = parseInt(document.getElementById("x").value);
-  var result = document.getElementById('result');
-  fetch('https://google.com?condition=x_in_range_a_b?a=' + a + '?b=' + b + '?x=' + x + '?result=' + result) .then(response => response.json()) .then(data => console.log(data)) .catch(error => console.error(error)); 
+  var result = document.getElementById('result').innerHTML;
+  if (result == 'Верно'){
+    result = true
+  } else {
+    result = false
+  }
+  fetch('https://google.com?condition=x_in_range_a_b&a=' + a + '&b=' + b + '&x=' + x + '&result=' + result)
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.error(error));
+  console.log("ok")
+  var msg = document.getElementById("message")
+  msg.innerHTML = 'Отправлено'
 }
+
+
